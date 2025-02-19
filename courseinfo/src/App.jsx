6171d7afb13cfd34cf9da20ff3524c6af741a875
Dennis Behnke <mail@dbehnke.com>
2025-02-19 10:32:24 +0800

    1.4 step 3 solved

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -15,6 +15,8 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props.part)
+  console.log(props.exercise)
   return (
     <>
       <Part ppart={props.part} pex={props.exercise}/>
@@ -33,20 +35,28 @@ const Footer = (props) => {
 const App = () => {
   // const-definitions
   const course = 'Half stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name:'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
+  // console.log(part1.name)
+  // console.log(part1.exercises)
 
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercise={exercises1}/>
-      <Content part={part2} exercise={exercises2}/>
-      <Content part={part3} exercise={exercises3}/>
-      <Footer ex={exercises1+exercises2+exercises3}/>
+      <Content part={part1.name} exercise={part1.exercises}/>
+      <Content part={part2.name} exercise={part2.exercises}/>
+      <Content part={part3.name} exercise={part3.exercises}/>
+      <Footer ex={part1.exercises+part2.exercises+part3.exercises}/> 
     </div>
   )
 }
